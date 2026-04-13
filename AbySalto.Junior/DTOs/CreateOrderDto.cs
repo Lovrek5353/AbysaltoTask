eaaf901bd2c99ca4767c01e84f2a7785af367533
Lovrek5353 <paladinic.l@gmail.com>
2026-04-13 12:20:37 +0200

    Added DTO objects

---
+++ AbySalto.Junior/DTOs/CreateOrderDto.cs
@@ -0,0 +1,19 @@
+﻿namespace AbySalto.Junior.DTOs
+{
+    public class CreateOrderDto
+    {
+        public string CustomerName { get; set; }
+        public string PaymentMethod { get; set; }
+        public string ShippingAddress { get; set; }
+        public string ContactNumber { get; set; }
+        public string? Note { get; set; }
+        public List<CreateOrderItemDto> Items { get; set; } = new();
+    }
+
+    public class CreateOrderItemDto
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
